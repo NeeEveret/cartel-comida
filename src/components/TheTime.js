b94d77b3211d716927f2511cf93894884bd84d3c
Kodama <darkveidery0.5@gmail.com>
2021-09-22 17:28:35 -0500

    Mas  texto en el componenteTheTime

--- src/components/TheTime.js
+++ src/components/TheTime.js
@@ -3,7 +3,12 @@ import {Component} from 'react'
 export default class TheTime extends Component {
     render(){
         return(
-            <h1>Aca este "TheTime"</h1>
+            <div>
+                <h1>Aca este "TheTime"</h1>
+                <p>Este componente se eliminara porque solo necesito una zona donde poner
+                     imagen
+                </p>
+            </div>
         )
     }
 }
